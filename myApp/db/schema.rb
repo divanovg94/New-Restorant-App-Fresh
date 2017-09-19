34151ac4f58d67d7de0ccec54364f993be870e32
divanovg94 <divanovg94@gmail.com>
2017-09-19 16:18:44 +0300

    DB updated

--- myApp/db/schema.rb
+++ myApp/db/schema.rb
@@ -10,16 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912112727) do
+ActiveRecord::Schema.define(version: 20170919123220) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
+     t.string "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "user_first_name"
     t.string "user_last_name"
+    
   end
 
   create_table "users", force: :cascade do |t|
